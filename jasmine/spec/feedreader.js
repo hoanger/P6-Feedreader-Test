45bd6ab7e3a309911587b08fd62c72871d7ffcd9
Andy Nguyen <nguyenandyh@gmail.com>
2016-03-10 19:12:03 -0800

    docs: Document URL and Name tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -16,10 +16,7 @@ $(function() {
     describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
+         * empty.
          */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
@@ -27,9 +24,8 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
+        /* This tests to make sure that each feed object in allFeeds
+         * has a URL defined and that the URL is not empty.
          */
          it('have URLs', function() {
             for (var i = allFeeds.length - 1; i >= 0; i--) {
@@ -38,9 +34,8 @@ $(function() {
             };
          });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
+        /* This tests to make sure that each feed object in allFeeds
+         * has a name defined and that the name is not empty.
          */
          it('are named', function() {
             for (var i = allFeeds.length - 1; i >= 0; i--) {
